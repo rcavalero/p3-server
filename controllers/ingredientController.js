d5260560ff0e60f8d80adc82005871789d547c27
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-02-16 12:40:22 -0800

    add asin4 to db.  add route to get one ingredient.

--- controllers/ingredientController.js
+++ controllers/ingredientController.js
@@ -8,6 +8,17 @@ router.get("/api/ingredients", (req, res) => {
     })
 })
 
+router.get("/api/ingredients/:id", (req, res) => {
+    db.Ingredient.findOne(
+        {
+            where: {
+                id: req.params.id
+            }
+        }).then(data => {
+            res.json(data)
+        })
+})
+
 router.post("/api/ingredients", (req, res) => {
     db.Ingredient.create(req.body).then(data => {
         res.json(data)
@@ -15,8 +26,6 @@ router.post("/api/ingredients", (req, res) => {
 })
 
 router.put("/api/ingredients/:id", (req, res) => {
-    console.log(req.body);
-    
     db.Ingredient.update(
         req.body,
         {
@@ -28,7 +37,7 @@ router.put("/api/ingredients/:id", (req, res) => {
         })
 })
 
-router.delete("/api/ingredients/delete/:id", (req, res) => {
+router.delete("/api/ingredients/:id", (req, res) => {
     db.Ingredient.destroy({
         where: {
             id: req.params.id
