fbba2e870547906e6ec446180456c2bf5065f9bc
Sarah <sarkwon89@gmail.com>
2020-02-17 16:48:54 -0800

    adding heroku config

--- controllers/ingredientController.js
+++ controllers/ingredientController.js
@@ -47,4 +47,24 @@ router.delete("/api/ingredients/:id", (req, res) => {
     })
 })
 
+router.get('/seedit',(req,res)=>{
+    const ingredients = [
+        {
+            "ingredient": "Salt",
+            "asin1": "B06XPN2785",
+            "asin2": "B00IZL2572",
+            "asin3": "B000Q3E8EA"
+        },
+        {
+            "ingredient": "Black Pepper",
+            "asin1": "B07VXKYTC3",
+            "asin2": "B074H5LYJN",
+            "asin3": "B0014E840Y"
+        }
+    ]
+    db.Ingredient.bulkCreate(ingredients).then(data => {
+        console.log("dataSeeded");
+    })
+})
+
 module.exports = router;
