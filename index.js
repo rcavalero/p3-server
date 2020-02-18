4e60988570d26888c4a6be7afd7789b8945ccad2
Sarah <sarkwon89@gmail.com>
2020-02-17 16:13:55 -0800

    deploy

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const cors = require("cors");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(cors({
-    origin:["http://localhost:3000"]
-}));
 // app.use(cors({
-//     origin:["https://seefood.herokuapp.com"]
+//     origin:["http://localhost:3000"]
 // }));
+app.use(cors({
+    origin:["https://seefoodar.herokuapp.com"]
+}));
 
 // Requiring our models for syncing
 var db = require('./models');
