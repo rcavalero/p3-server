8ef34b2e536f4715f620042ca15883bc3940d042
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-02-25 11:53:20 -0800

    add authentication

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const session = require("express-session")
+
+
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// app.use(cors({
-//     origin:["http://localhost:3000"]
-// }));
 app.use(cors({
-    origin:["https://seefood-app.herokuapp.com"]
+    origin:["http://localhost:3000"],
+    credentials: true 
 }));
+
+app.use(session({ secret: "something secret here", resave: true, saveUninitialized: true,cookie:{maxAge: 7200000} }));
+
+// app.use(cors({
+//     origin:["https://seefood-app.herokuapp.com"],
+    // credentials: true 
+// }));
 
 // Requiring our models for syncing
 var db = require('./models');
@@ -23,6 +32,9 @@ app.use(express.json());
 
 const ingredientController = require("./controllers/ingredientController");
 app.use(ingredientController)
+const userController = require("./controllers/userController");
+app.use(userController)
+
 
 db.sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
